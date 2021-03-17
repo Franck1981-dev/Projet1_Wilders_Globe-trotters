35bd3a9b44b9b1ec163d8bffc689120a119a2216
Keisuke Joulia <keisukejoulia@yahoo.co.jp>
2021-03-17 18:41:22 +0100

    Débug du code

--- layout/_header.php
+++ layout/_header.php
@@ -3,15 +3,15 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Wilders Globe-Trotters/title>
-    <link rel="stylesheet" href="style.css">
+    <title>Wilders Globe-Trotters</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body>
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="tokyo.php">Tokyo</a></li>
+            <li><a href="japon.php">Japon</a></li>
             <li><a href="jordanie.php">Jordanie</a></li>
             <li><a href="lareunion.php">La Réunion</a></li>
             <li><a href="contact.php">Contactez-moi</a></li>
