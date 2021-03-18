6dce2c434e94faee8212a21ed3987b66ac93ae24
JANSONkevin <kevin.janson.pro@gmail.com>
2021-03-18 09:23:16 +0100

    ajustement des articles

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include 'layout/_header.php';?>
-
-    <img src="https://picsum.photos/203" id="logo" alt="logo"/>
-
+    <div>
+        <img src="https://picsum.photos/203" id="logo" alt="logo"/>
+        <p>[Les voyages des wilders Globe-trotters]</p>
+        <p>Les pays que l'on veut partager !</p>
+    </div>
     <section class="line">
         <h1 class="titre">Accueil</h1>
 
@@ -15,45 +17,38 @@
         <h2 class="titre">Les pays visités</h2>
         </article>
         <div class="cards">
-        <div class="card animate__animated animate__backInLeft">
             <div class="card_content">
-                <div class="card_picture">
-                    <img src="https://photos.tui.fr/vnf/Produits/JORCT011_mer-morte-circuits-jordanie-tui.jpg" alt="jordanie">
-                </div>
-                <div class="card_title">
-                    <h3>Jordanie</h3>
-                </div>
-                <div class="card_footer">
-                    <a href="jordanie.php" class="btn-like">Découvrir</a>
-                </div>
+            <div class="card_picture">
+                <img
+                src="https://photos.tui.fr/vnf/Produits/JORCT011_mer-morte-circuits-jordanie-tui.jpg"
+                alt="jordanie"
+                />
+                <h3 class="card_title">Jordanie</h3>
+                <a href="jordanie.php" class="btn">Découvrir</a>
             </div>
-        </div>
-        <div class="card animate__animated animate__backInLeft">
+            </div>
+
             <div class="card_content">
-                <div class="card_picture">
-                    <img src=" https://www.noble-caledonia.co.uk/thumb.php?source_type=editable_pages&source_info%5Bmedia_gallery_item_id%5D=36717&source_info%5Burl%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Bsecure_url%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Brelative_url%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Balt%5D=Beach+at+Boucan+Canot+on+Reunion&source_info%5Bcaption%5D=Beach+at+Boucan+Canot+on+Reunion&source_info%5Bwidth%5D=800&source_info%5Bheight%5D=533&source_info%5Bfilesystem_path%5D=%2Fhome%2Fdev%2Fpublic%2FCWT%2FMediaGallery%2Fuploads%2FAB%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Btype%5D=JPEG+Image&source_info%5Bmime_type%5D=image%2Fjpeg&action=A.802.400" alt="la reunion">
-                </div>
-                <div class="card_title">
-                    <h3>La Réunion</h3>
-                </div>
-                <div class="card_footer">
-                    <a href="lareunion.php" class="btn-like">Découvrir</a>
-                </div>
+            <div class="card_picture">
+                <img
+                src=" https://www.noble-caledonia.co.uk/thumb.php?source_type=editable_pages&source_info%5Bmedia_gallery_item_id%5D=36717&source_info%5Burl%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Bsecure_url%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Brelative_url%5D=%2Fab%2Fuploads%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Balt%5D=Beach+at+Boucan+Canot+on+Reunion&source_info%5Bcaption%5D=Beach+at+Boucan+Canot+on+Reunion&source_info%5Bwidth%5D=800&source_info%5Bheight%5D=533&source_info%5Bfilesystem_path%5D=%2Fhome%2Fdev%2Fpublic%2FCWT%2FMediaGallery%2Fuploads%2FAB%2FNCD%2Fshutterstock_110491838.jpg&source_info%5Btype%5D=JPEG+Image&source_info%5Bmime_type%5D=image%2Fjpeg&action=A.802.400"
+                alt="la reunion"
+                />
+                <h3 class="card_title">La Réunion</h3>
+                <a href="lareunion.php" class="btn">Découvrir</a>
             </div>
-        </div>
-        <div class="card animate__animated animate__backInLeft">
+            </div>
+
             <div class="card_content">
-                <div class="card_picture">
-                    <img src=" https://images8.alphacoders.com/887/887586.jpg" alt="japon">
-                </div>
-                <div class="card_title">
-                    <h3>Japon</h3>
-                </div>
-                <div class="card_footer">
-                    <a href="japon.php" class="btn-like">Découvrir</a>
-                </div>
+            <div class="card_picture">
+                <img
+                src=" https://images8.alphacoders.com/887/887586.jpg"
+                alt="japon"
+                />
+                <h3 class="card_title">Japon</h3>
+                <a href="japon.php" class="btn">Découvrir</a>
             </div>
-        </div>
+            </div>
     </div>
     </section>
 
