0338e1160ab5b576c8f2f9cc4f7dc996dd743d35
Alexandre Morlat <alexandre-morlat_student2021@wilder.school>
2021-03-18 18:04:48 +0100

    Updates style.css _header.php and _footer.php for Desktop version

--- layout/_footer.php
+++ layout/_footer.php
@@ -1,4 +1,21 @@
-<footer>
+<footer class="socialMedia">
+  <!--<div class="firstRowFooter">-->
+        <div class="textMediaFooter">
+            <h3>Retrouvez-nous sur :</h3>
+        </div>
+   <!-- </div>-->
+    <!--<div class="secondRowFooter">-->
+        <div class="icones">
+            <img src="images/facebook.png" alt="facebook"></img>
+            <img src="images/snapchat.png" alt="snapchat"></img>
+            <img src="images/instagram.png" alt="instagram"></img>
+            <img src="images/twitter.png" alt="twitter"></img>
+        </div>
+        <div class="mentions">
+            <h5>@Wilders Globe Trotters</h5>
+        </div>
+    <!--</div>-->
 </footer>
 </body>
+
 </html>
