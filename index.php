e6a5de72a7a58e7168893116e5f07fab14452769
Keisuke Joulia <keisukejoulia@yahoo.co.jp>
2021-03-24 18:49:18 +0100

    Ajout du texte 'qui sommes nous' + ajout du css du texte + modif du mobile.css sur le responsive de la page contacte qui avais cassé

--- index.php
+++ index.php
@@ -1,52 +1,57 @@
-<?php include 'layout/_header.php';?>
-    <!-- <div class="banner">
+<?php include 'layout/_header.php'; ?>
+<!-- <div class="banner">
         <img src="https://zupimages.net/up/21/11/g607.jpg" id="logo" alt="logo"/>
         <div class="banner_background">
         <p class="banner_title">[Les voyages des wilders Globe-trotters]</p>
         <p class="banner_text">Les pays que l'on veut partager !</p>
         </div>
     </div> -->
-    <div id="slider">
-      <figure>
+<div id="slider">
+    <figure>
         <img src="https://zupimages.net/up/21/12/sham.jpeg" alt />
         <img src="https://zupimages.net/up/21/12/vjjq.jpeg" alt />
         <img src="https://zupimages.net/up/21/12/ue42.jpeg" alt />
         <img src="https://zupimages.net/up/21/12/1771.jpeg" alt />
         <img src="https://zupimages.net/up/21/12/sham.jpeg" alt />
-      </figure>
-    </div>
-    <section class="line">
-        <h1 class="titre">Accueil</h1>
+    </figure>
+</div>
+<section class="line">
+    <h1 class="titre">Accueil</h1>
 
-    </section>
-    <section>
-        <h2 class="titre">Qui sommes-nous</h2>
-        <article class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolore odio placeat quidem, veritatis harum reprehenderit, ipsa non delectus quaerat corrupti obcaecati eius quo asperiores at laborum quibusdam cumque esse?</article>
-    </section>
-    <section>
-        <article class="line">
+</section>
+<section>
+    <h2 class="titre">Qui sommes-nous</h2>
+    <article class="text">Nous sommes un groupe de 5 Wilders globe-trotters composés de: Mickael, Alexandre, Kevin et Guillaume.<br> 
+        Nous nous sommes rencontrés autour d'un projet commun à la Wild Code School où nous avons décidé de vous faire partager notre expérience.<br>
+        Amoureux de voyage et désireux de faire partager aux plus curieux d'entre vous, vous trouverez en explorant notre site, toutes nos découvertes, 
+        astuces et informations sur les pays que nous avons visité.<br>
+        Une demande d'information sur un pays, une ville, comment s'y rendre, nous reporter un problème, n'hesitez pas à nous contacter !!<br>
+        L'équipe des Wilders globe-trotters vous souhaites une agréable navigation sur notre site !</article>
+</section>
+<section>
+    <article class="line">
         <h2 class="titre">Les pays visités</h2>
-        </article>
-        <article class="articles">
+    </article>
+    <article class="articles">
 
         <div class="cartes">
 
             <div class="carte" id="jordanie">
-            <h3 class="titre_carte">Jordanie</h3>
+                <h3 class="titre_carte">Jordanie</h3>
                 <a href="jordanie.php" class="btn">Découvrir</a>
             </div>
 
             <div class="carte" id="reunion">
-            <h3 class="titre_carte">La Réunion</h3>
+                <h3 class="titre_carte">La Réunion</h3>
                 <a href="lareunion.php" class="btn">Découvrir</a>
             </div>
 
             <div class="carte" id="japon">
-            <h3 class="titre_carte">Japon</h3>
+                <h3 class="titre_carte">Japon</h3>
                 <a href="japon.php" class="btn">Découvrir</a>
             </div>
 
         </div>
-        </article>
-    </section>
-<?php include 'layout/_footer.php';?>
+    </article>
+</section>
+<?php include 'layout/_footer.php'; ?>
